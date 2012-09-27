d894be067efd53baf305b120faf629bf5097c8ac
Michael Nugent <github11@illuminatus.org>
2012-09-26 23:47:02 -0700

    initial add of rails stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DNSinator::Application.config.secret_token = 'bedc92d68cd12389da63c74257e0e3f3f8bfa8c3da056d2ebe33245215a32e82a94cdf209ad62d004cd9e42bbd60c7c93fe05f8bbfbd3486861d7997fc818a7c'
